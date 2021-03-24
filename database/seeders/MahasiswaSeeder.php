392267242a2505bd2670c54f58690c756e09a9f7
NadiaLayraAziza <nadialayra15@gmail.com>
2021-03-24 15:02:57 +0700

    Meng-insert data awal melalui seeder

--- database/seeders/MahasiswaSeeder.php
+++ database/seeders/MahasiswaSeeder.php
@@ -13,6 +13,28 @@ class MahasiswaSeeder extends Seeder
      */
     public function run()
     {
-        //
+        \App\Models\Mahasiswa::insert([
+            [
+                'Nim' => '1941720061',
+                'Nama' => 'Nadia Layra Aziza',
+                'Kelas' => 'TI-2C',
+                'Jurusan' => 'Teknologi Informasi',
+                'No_Handphone' => '081259347608'
+            ],
+            [
+                'Nim' => '2041720007',
+                'Nama' => 'Najma Faradisa',
+                'Kelas' => 'AK-1E',
+                'Jurusan' => 'Akuntansi',
+                'No_Handphone' => '088000451175'
+            ],
+            [
+                'Nim' => '1731720125',
+                'Nama' => 'Ahmad Uwais Al-Qorni',
+                'Kelas' => 'TS-4B',
+                'Jurusan' => 'Tknik Sipil',
+                'No_Handphone' => '081234489551'
+            ]
+        ]);
     }
 }
