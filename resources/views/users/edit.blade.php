857016744efc59e8cbb223ff2f63c56f988f10d6
NadiaLayraAziza <nadialayra15@gmail.com>
2021-03-24 23:58:39 +0700

    Tugas 1 - Step 6 Mengedit file view edit untuk menambahkan form Tanggal_Lahir dan Email pada saat edit data

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -32,6 +32,10 @@
                         <input type="text" name="Nama" class="form-control" id="Nama" value="{{ $Mahasiswa->Nama }}" aria-describedby="Nama" > 
                     </div>
                     <div class="form-group">
+                        <label for="Tanggal_Lahir">Tanggal_Lahir</label> 
+                        <input type="date" name="Tanggal_Lahir" class="form-control" id="Tanggal_Lahir" value="{{ $Mahasiswa->Tanggal_Lahir }}" aria-describedby="Tanggal_Lahir" > 
+                    </div>
+                    <div class="form-group">
                         <label for="Kelas">Kelas</label> 
                         <input type="Kelas" name="Kelas" class="form-control" id="Kelas" value="{{ $Mahasiswa->Kelas }}" aria-describedby="Kelas" > 
                     </div>
@@ -42,6 +46,10 @@
                     <div class="form-group">
                         <label for="No_Handphone">No_Handphone</label>  
                         <input type="No_Handphone" name="No_Handphone" class="form-control" id="No_Handphone" value="{{ $Mahasiswa->No_Handphone }}" aria-describedby="No_Handphone" > 
+                    </div>
+                    <div class="form-group">
+                        <label for="Email">Email</label> 
+                        <input type="email" name="Email" class="form-control" id="Email" value="{{ $Mahasiswa->Email }}" aria-describedby="Email" > 
                     </div>
                 <button type="submit" class="btn btn-primary">Submit</button>
                 </form>
